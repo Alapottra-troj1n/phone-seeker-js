81264ab8f5d1eef87b28c35ed07d1ccc168746c7
Alapottra <alapottratrojon@gmail.com>
2022-02-28 23:16:35 +0600

    added fetch data function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 const searchBtn = document.querySelector('.search-btn');
 
+
+
+
+
+//fetching data function
+
+const getData = async(searchValue) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`);
+    const data = await response.json();
+    console.log(data.data);
+}
+
+
+
+//button event listener 
+
 searchBtn.addEventListener('click',()=>{
 
-    console.log('hello')
+    const searchValue = document.querySelector('.search-input').value;
+    getData(searchValue.toLowerCase());
 
 });
+
+
